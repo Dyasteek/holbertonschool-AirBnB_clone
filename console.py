891cda734c6dd56d50aac3990e043e5b70ec244e
AnitRodriguez <anitruthrodriguez@gmail.com>
2024-02-27 07:20:01 -0800

    pycodestyle solutions

--- console.py
+++ console.py
@@ -4,24 +4,24 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-	"""Command interpreter prompt"""
-	
-	prompt = '(hbnb)'
-	
-	def emptyline(self):
-		"""Does nothing when it receibes an empty line"""
-		pass
-	
-	def do_quit(self, *args):
-		"""Quit command to exit the program"""
-		return True
-	
-	def do_EOF(self, *args):
-		"""EOF command to exit the program"""
-		return True
-	def do_help(self, *args):
-		"""Help command"""
-		print("Quit command to exit the program")
+    """Command interpreter prompt"""
+
+    prompt = '(hbnb)'
+
+    def emptyline(self):
+        """Does nothing when it receibes an empty line"""
+        pass
+
+    def do_quit(self, *args):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, *args):
+        """EOF command to exit the program"""
+        return True
+    def do_help(self, *args):
+        """Help command"""
+        print("Quit command to exit the program")
 
 if __name__ == '__main__':
-	HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
