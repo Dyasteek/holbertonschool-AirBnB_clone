44bbfaf5208778218f83a6e080cf7e3c6df32312
AnitRodriguez <anitruthrodriguez@gmail.com>
2024-02-27 07:28:29 -0800

    final solucion pycodestyle

--- console.py
+++ console.py
@@ -19,9 +19,11 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, *args):
         """EOF command to exit the program"""
         return True
+
     def do_help(self, *args):
         """Help command"""
         print("Quit command to exit the program")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
