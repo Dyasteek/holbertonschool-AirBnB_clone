7dfa9a31c797f96b50a46c522ed6b38ef35e2ef8
DST-Gateway <lawarsenik@gmail.com>
2024-02-27 15:06:21 -0300

    Base model initial

---
+++ models/base_model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Base Model"""
+import uuid
+import datetime
+
+class BaseModel:
+    """Base Model Class"""
+    
+    def __init__(self):
+        """Initialization generating id and datetime"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        
+    def update_timestamp(self):
+        """Update datetime"""
+        self.updated_at = datetime.now()
