8ffa0897a5f714775ca0cb6068df11c1817c7320
DST-Gateway <lawarsenik@gmail.com>
2024-02-28 11:17:19 -0300

    Upgraded methods

--- console.py
+++ console.py
@@ -6,55 +6,61 @@ from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter prompt"""
+    """Command interpreter for HBnB"""
 
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
 
     def emptyline(self):
-        """Does nothing when it receibes an empty line"""
+        """Does nothing when it recieves an empty line."""
         pass
 
     def do_quit(self, *args):
-        """Quit command to exit the program"""
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, *args):
-        """EOF command to exit the program"""
+        """EOF command to exit the program."""
         return True
-
+    
     def do_help(self, *args):
         """Help command"""
         print("Quit command to exit the program")
-        
+
     def do_create(self, *args):
-        """Create a new instance of BaseModel subclass"""
-        if not args:
+        """Creates a new instance of an object.
+        Usage: create class_name"""
+        arg = args[0].split()
+        if len(arg) == 0:
             print("** class name missing **")
-            return
-        args = args.split()
-        if args != "BaseModel":
+        elif arg[0] not in HBNBCommand.models:
             print("** class doesn't exist **")
-            return
         else:
-            model = BaseModel()
-            print(model.id)
-        
-    def do_show(self, *args):
-        """Show all instances of a given class"""
-        model = BaseModel()
-        if not args:
-            print("** class name missing **")
-            return
-        if args[0] != "BaseModel":
+            class_name = arg[0]
+        if class_name in globals():
+            obj = globals()[class_name]()
+            obj.save()
+            print(obj.id)
+        else:
             print("** class doesn't exist **")
-            return
-        if len(args) < 2:
+
+    def do_show(self, *args):
+        """Prints the string representation of an instance.
+        Usage: show class_name id"""
+        arg = args[0].split()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.models:
+            print("** class doesn't exist **")
+        elif len(arg) < 2:
             print("** instance id missing **")
-            return
-        if args[1] != model.id:
-            model_id = args[1]
-        if model_id not in storage.get(args[0], model_id):
-            print("** no instance found **")
+        else:
+            obj_dict = storage.all()
+            key = "{}.{}".format(arg[0], arg[1])
+            if key not in obj_dict:
+                print("** no instance found **")
+            else:
+                print(obj_dict[key])
+                
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
