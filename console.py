12d66916fa206ff21d1c32c6542bb858af7dff77
DST-Gateway <lawarsenik@gmail.com>
2024-02-28 03:48:56 -0300

    Added new instances

--- console.py
+++ console.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Command interpreter for HBnB"""
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter promptt"""
+    """Command interpreter prompt"""
 
     prompt = '(hbnb)'
 
@@ -23,7 +25,36 @@ class HBNBCommand(cmd.Cmd):
     def do_help(self, *args):
         """Help command"""
         print("Quit command to exit the program")
-
+        
+    def do_create(self, *args):
+        """Create a new instance of BaseModel subclass"""
+        if not args:
+            print("** class name missing **")
+            return
+        args = args.split()
+        if args != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        else:
+            model = BaseModel()
+            print(model.id)
+        
+    def do_show(self, *args):
+        """Show all instances of a given class"""
+        model = BaseModel()
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        if args[1] != model.id:
+            model_id = args[1]
+        if model_id not in storage.get(args[0], model_id):
+            print("** no instance found **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
